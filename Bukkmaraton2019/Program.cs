71caaa8d47bf75a627aaa33ac5cf5d5aa9b8932f
kopasz.bela <kopasz.bela@TANARIGEP-001>
2026-05-14 12:22:50 +0200

    Add project files.

---
+++ Bukkmaraton2019/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bukkmaraton2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdatBeolvas();
+        }
+
+        private static void AdatBeolvas()
+        {
+            using (StreamReader olvas = new StreamReader("bukkm2019.txt"))
+            {
+                olvas.ReadLine();
+                while (!olvas.EndOfStream)
+                {
+                    string[] sor = olvas.ReadLine().Split(';');
+                    Versenyzo egyVersenyzo = new Versenyzo(sor[0],sor[1],sor[2],sor[3],TimeSpan.Parse(sor[4]));
+                }
+            }
+        }
+    }
+}
---
+++ Bukkmaraton2019/Versenyzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bukkmaraton2019
+{
+    internal class Versenyzo
+    {
+        public Versenyzo(string rajtszam, string kategoria, string nev, string egyesulet, TimeSpan ido)
+        {
+            Rajtszam = rajtszam;
+            Kategoria = kategoria;
+            Nev = nev;
+            Egyesulet = egyesulet;
+            Ido = ido;
+        }
+
+        /*Rajtszám;Kategória;Név;Egyesület;Idő
+M107;m3f;Ács Zoltán;;1:21:25*/
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+    }
+}
